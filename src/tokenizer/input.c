55cdeb55b2cbd59cec9b80162d00e76e01f5b6da
yagruda <gggccclas@proton.me>
2025-10-21 15:31:53 +0200

    transfer from home to 42workstation

--- src/tokenizer/input.c
+++ src/tokenizer/input.c
@@ -6,25 +6,27 @@
 /*   By: yhruda <yhruda@student.42warsaw.pl>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 16:42:23 by yhruda            #+#    #+#             */
-/*   Updated: 2025/10/19 20:39:54 by yhruda           ###   ########.fr       */
+/*   Updated: 2025/10/21 14:56:10 by yhruda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int input_is_valid(char* input)
+int input_is_valid(char* input, t_shell *shell)
 {
 	if (!quotes_closed(input))
 	{
 		custom_error("syntax error: unclosed quotes\n");
-		// TBD: shell->exit_code = 258; 
+		shell->exit_code = 258; 
+		printf("exit code set to %d\n", shell->exit_code);
 		// TBD: mark is_valid = 0 in shell input struct
 		return 0;
 	}
 	if (is_empty(input))
 	{
 		custom_error("syntax error: empty input\n");
-		// TBD: shell->exit_code = 258;
+		shell->exit_code = 258;
+		printf("exit code set to %d\n", shell->exit_code);
 		// TBD: mark is_valid = 0 in shell input struct
 		return 0;
 	}
@@ -40,7 +42,7 @@ void process_input(t_shell *shell, char* input, char** args)
 	(void) shell;
 	(void) args;
 
-	if (input_is_valid(input))
+	if (input_is_valid(input, shell))
 	{
 		add_history(input);
 		// TBD: tokenizer(shell,input);
@@ -52,9 +54,6 @@ void process_input(t_shell *shell, char* input, char** args)
 	}
 	
 	// TBD: call parser here
-	
-	
-	
 
 	// FOR DEBUGGING ONLY, DELETE LATER
 	if (ft_strcmp(input, "") != 0)
