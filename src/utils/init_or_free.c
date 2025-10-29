88452cf5d938bce2eebe57f8840fff66a11ff109
yagruda <gggccclas@proton.me>
2025-10-29 17:47:33 +0100

    final

--- src/utils/init_or_free.c
+++ src/utils/init_or_free.c
@@ -12,6 +12,35 @@
 
 #include "../minishell.h"
 
+static char	**dup_envp(char **envp)
+{
+	char	**new_envp;
+	int		i;
+	int		len;
+
+	len = 0;
+	while (envp[len])
+		len++;
+	new_envp = malloc(sizeof(char *) * (len + 1));
+	if (!new_envp)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		new_envp[i] = ft_strdup(envp[i]);
+		if (!new_envp[i])
+		{
+			while (i-- > 0)
+				free(new_envp[i]);
+			free(new_envp);
+			return (NULL);
+		}
+		i++;
+	}
+	new_envp[i] = NULL;
+	return (new_envp);
+}
+
 t_shell	*init_shell(char **envp)
 {
 	t_shell	*shell;
@@ -24,9 +53,30 @@ t_shell	*init_shell(char **envp)
 	}
 	shell->tokens = NULL;
 	shell->cmd_list = NULL;
-	shell->envp = envp;
+	shell->envp = dup_envp(envp);
+	if (!shell->envp)
+	{
+		custom_error("Memory allocation for envp failed\n");
+		free(shell);
+		exit(EXIT_FAILURE);
+	}
 	shell->exit_code = 0;
 	return (shell);
+}
+
+static void	free_envp(char **envp)
+{
+	int	i;
+
+	if (!envp)
+		return ;
+	i = 0;
+	while (envp[i])
+	{
+		free(envp[i]);
+		i++;
+	}
+	free(envp);
 }
 
 void	free_shell(t_shell *shell)
@@ -35,5 +85,6 @@ void	free_shell(t_shell *shell)
 		return ;
 	free_tokens(shell->tokens);
 	free_commands(shell->cmd_list);
+	free_envp(shell->envp);
 	free(shell);
 }
