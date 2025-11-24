25e1129cf81d9264b4b96192ebfb467a7b3b891c
yagruda <gggccclas@proton.me>
2025-11-24 22:37:20 +0100

    fix for mi

--- src/executor/redirects.c
+++ src/executor/redirects.c
@@ -12,53 +12,72 @@
 
 #include "executor.h"
 
-static void	handle_input_redir(char *file, t_shell *shell)
-{
-	int	fd;
+static void handle_input_redir(char *file, t_shell *shell) {
+  int fd;
 
-	fd = open(file, O_RDONLY);
-	if (fd == -1)
-	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(file, 2);
-		ft_putstr_fd(": No such file or directory\n", 2);
-		shell->exit_code = 1;
-		exit(1);
-	}
-	dup2(fd, STDIN_FILENO);
-	close(fd);
+  fd = open(file, O_RDONLY);
+  if (fd == -1) {
+    ft_putstr_fd("minishell: ", 2);
+    ft_putstr_fd(file, 2);
+    ft_putstr_fd(": No such file or directory\n", 2);
+    shell->exit_code = 1;
+    exit(1);
+  }
+  dup2(fd, STDIN_FILENO);
+  close(fd);
 }
 
-static void	handle_output_redir(char *file, int append, t_shell *shell)
-{
-	int	fd;
+static void handle_output_redir(char *file, int append, t_shell *shell) {
+  int fd;
 
-	if (append)
-		fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-	else
-		fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (fd == -1)
-	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(file, 2);
-		ft_putstr_fd(": Permission denied\n", 2);
-		shell->exit_code = 1;
-		exit(1);
-	}
-	dup2(fd, STDOUT_FILENO);
-	close(fd);
+  if (append)
+    fd = open(file, O_WRONLY | O_CREAT | O_APPEND, 0644);
+  else
+    fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (fd == -1) {
+    ft_putstr_fd("minishell: ", 2);
+    ft_putstr_fd(file, 2);
+    ft_putstr_fd(": Permission denied\n", 2);
+    shell->exit_code = 1;
+    exit(1);
+  }
+  dup2(fd, STDOUT_FILENO);
+  close(fd);
 }
 
-void	setup_redirects(t_redir *redir, t_shell *shell)
-{
-	while (redir)
-	{
-		if (redir->type == REDIRECT_IN)
-			handle_input_redir(redir->file, shell);
-		else if (redir->type == REDIRECT_OUT)
-			handle_output_redir(redir->file, 0, shell);
-		else if (redir->type == APPEND)
-			handle_output_redir(redir->file, 1, shell);
-		redir = redir->next;
-	}
+static void handle_heredoc(char *delimiter) {
+  int fd[2];
+  char *line;
+
+  if (pipe(fd) == -1) {
+    perror("minishell");
+    exit(1);
+  }
+  while (1) {
+    line = readline("> ");
+    if (!line || ft_strcmp(line, delimiter) == 0) {
+      free(line);
+      break;
+    }
+    ft_putstr_fd(line, fd[1]);
+    ft_putstr_fd("\n", fd[1]);
+    free(line);
+  }
+  close(fd[1]);
+  dup2(fd[0], STDIN_FILENO);
+  close(fd[0]);
+}
+
+void setup_redirects(t_redir *redir, t_shell *shell) {
+  while (redir) {
+    if (redir->type == REDIRECT_IN)
+      handle_input_redir(redir->file, shell);
+    else if (redir->type == REDIRECT_OUT)
+      handle_output_redir(redir->file, 0, shell);
+    else if (redir->type == APPEND)
+      handle_output_redir(redir->file, 1, shell);
+    else if (redir->type == HEREDOC)
+      handle_heredoc(redir->file);
+    redir = redir->next;
+  }
 }
--- src/executor/single_cmd.c
+++ src/executor/single_cmd.c
@@ -10,63 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "executor.h"
 #include "../builtins/builtins.h"
+#include "executor.h"
 
-static void	execute_external_cmd(t_cmd *cmd, t_shell *shell)
-{
-	char	*path;
+static void execute_external_cmd(t_cmd *cmd, t_shell *shell) {
+  char *path;
 
-	path = find_command(cmd->argv[0], shell->envp);
-	if (!path)
-	{
-		ft_putstr_fd("minishell: ", 2);
-		ft_putstr_fd(cmd->argv[0], 2);
-		ft_putstr_fd(": command not found\n", 2);
-		exit(127);
-	}
-	execve(path, cmd->argv, shell->envp);
-	ft_putstr_fd("minishell: ", 2);
-	ft_putstr_fd(cmd->argv[0], 2);
-	ft_putstr_fd(": command execution failed\n", 2);
-	free(path);
-	exit(126);
+  path = find_command(cmd->argv[0], shell->envp);
+  if (!path) {
+    ft_putstr_fd("minishell: ", 2);
+    ft_putstr_fd(cmd->argv[0], 2);
+    ft_putstr_fd(": command not found\n", 2);
+    free_shell(shell);
+    exit(127);
+  }
+  execve(path, cmd->argv, shell->envp);
+  ft_putstr_fd("minishell: ", 2);
+  ft_putstr_fd(cmd->argv[0], 2);
+  ft_putstr_fd(": command execution failed\n", 2);
+  free(path);
+  free_shell(shell);
+  exit(126);
 }
 
-static void	exec_in_child(t_cmd *cmd, t_shell *shell)
-{
-	setup_redirects(cmd->redir, shell);
-	if (is_builtin(cmd->argv[0]))
-	{
-		shell->exit_code = exec_builtin(cmd, shell);
-		exit(shell->exit_code);
-	}
-	execute_external_cmd(cmd, shell);
+static void exec_in_child(t_cmd *cmd, t_shell *shell) {
+  signal(SIGQUIT, SIG_DFL);
+  setup_redirects(cmd->redir, shell);
+  if (is_builtin(cmd->argv[0])) {
+    shell->exit_code = exec_builtin(cmd, shell);
+    free_shell(shell);
+    exit(shell->exit_code);
+  }
+  execute_external_cmd(cmd, shell);
 }
 
-void	exec_single_cmd(t_cmd *cmd, t_shell *shell)
-{
-	pid_t	pid;
-	int		status;
+void exec_single_cmd(t_cmd *cmd, t_shell *shell) {
+  pid_t pid;
+  int status;
 
-	if (!cmd->argv || !cmd->argv[0])
-		return ;
-	if (is_builtin(cmd->argv[0]) && !cmd->redir)
-	{
-		shell->exit_code = exec_builtin(cmd, shell);
-		return ;
-	}
-	pid = fork();
-	if (pid == -1)
-	{
-		ft_putstr_fd("minishell: fork failed\n", 2);
-		return ;
-	}
-	if (pid == 0)
-		exec_in_child(cmd, shell);
-	waitpid(pid, &status, 0);
-	if (WIFEXITED(status))
-		shell->exit_code = WEXITSTATUS(status);
-	else if (WIFSIGNALED(status))
-		shell->exit_code = 128 + WTERMSIG(status);
+  if (!cmd->argv || !cmd->argv[0])
+    return;
+  if (is_builtin(cmd->argv[0]) && !cmd->redir) {
+    shell->exit_code = exec_builtin(cmd, shell);
+    return;
+  }
+  pid = fork();
+  if (pid == -1) {
+    ft_putstr_fd("minishell: fork failed\n", 2);
+    return;
+  }
+  if (pid == 0)
+    exec_in_child(cmd, shell);
+  waitpid(pid, &status, 0);
+  if (WIFEXITED(status))
+    shell->exit_code = WEXITSTATUS(status);
+  else if (WIFSIGNALED(status))
+    shell->exit_code = 128 + WTERMSIG(status);
 }
